7c046e23000f04cd60de96dd020b8fe0135e363f
strigkos <31070654+strigkos@users.noreply.github.com>
2017-09-30 23:18:43 +0300

    Update BaseController_Strigkos.php

--- BaseController_Strigkos.php
+++ BaseController_Strigkos.php
@@ -88,10 +88,10 @@ class BaseController {
 }
 
 // DB Credentials
-$db_host = 'localhost';
-$db_user = 'root';
+$db_host = '';
+$db_user = '';
 $db_pass = '';
-$db_name = 'psi_test';
+$db_name = '';
 
 // DB Connection
 $db = new PDO("mysql:host=" . $db_host . ";dbname=" . $db_name, $db_user, $db_pass);
